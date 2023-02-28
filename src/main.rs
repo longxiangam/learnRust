72128265252573e64af61e383515cf6e39bf4007
longxiangam <longxiangam@163.com>
2023-02-28 18:12:38 +0800

    999

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #![allow(non_snake_case)]
+
+use std::io::{Stdin, stdin, stdout, Write};
+use std::thread::Thread;
+
 mod dir;
 mod ptr;
 
 fn main() {
     println!("Hello, world!");
-   /* dir::print_dir("/");
-    dir::read_file();*/
+    let stdin = stdin();
+    let mut readLine = String::new();
+    stdin.read_line(&mut readLine);
 
-    ptr::test_ptr();
+    let mut stdout = stdout();
+    stdout.write(readLine.as_bytes());
+
+    let i = 1;
+    let j = 1;
+    for i in 1..10 {
+        for j in i..10 {
+            print!("{} * {} = {};  ",i,j,i * j);
+        }
+        println!("");
+    }
 
 }
