95c836000143303bb55ba05bffe471c457e31066
longxiangam <longxiangam@163.com>
2023-02-28 01:37:42 +0000

    999

--- src/guess.rs
+++ src/guess.rs
@@ -5,14 +5,16 @@ use rand::Rng;
 pub fn start(){
     let mut colors = [-1 ; 4];
     
-    for mut o in colors  {
-        let mut temp_v = -1 ;
-
+    for i in 0..4 {
+        let mut temp_v = 0 ;
         while colors.contains(&temp_v) {
-           temp_v =  rand::thread_rng().gen_range(0..3);
+           temp_v =  rand::thread_rng().gen_range(0..4);
         }
+        colors[i] = temp_v;
+        println!("{}",temp_v);
 
-        o = temp_v;
-        println!("{}",o);
     }
+
+    
+    
 }
